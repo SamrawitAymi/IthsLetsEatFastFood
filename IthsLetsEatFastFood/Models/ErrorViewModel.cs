a9db310076113dfaa136ed48c364818d399e0282
Samrawit Haylemeskel <samrieb1980@gmail.com>
2020-04-22 00:06:55 +0200

    Add project files.

---
+++ IthsLetsEatFastFood/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IthsLetsEatFastFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

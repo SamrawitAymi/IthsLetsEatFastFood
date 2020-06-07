db100b3a74a06a4ce80efcd2e50f67405d2a876f
Samrawit Haylemeskel <samrieb1980@gmail.com>
2020-06-08 00:46:02 +0200

    Removed unused command

--- IthsLetsEatFastFood/Services/ChangeService/OrderService.cs
+++ IthsLetsEatFastFood/Services/ChangeService/OrderService.cs
@@ -1,15 +1,9 @@
-﻿
-using IthsLetsEatFastFood.Models;
-using IthsLetsEatFastFood.Services.QueryService;
-using IthsLetsEatFastFood.ViewModel;
+﻿using IthsLetsEatFastFood.ViewModel;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace IthsLetsEatFastFood.Services.ChangeService
 {

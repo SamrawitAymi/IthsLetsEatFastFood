f7bc8c44e6c5325b18f65aceddd3a20ab1fea8bd
Samrawit Haylemeskel <samrieb1980@gmail.com>
2020-06-09 14:53:12 +0200

    Add new migration and add data foodProducts tables in a database

---
+++ Lets.OrderWebService/FoodProducts.sql
@@ -0,0 +1,16 @@
+USE [OrderDatabase]
+
+INSERT [dbo].[FoodProducts] ([Id], [Name], [Description], [Price], [ImageUrl]) VALUES (N'8f645fea-fd49-4e5c-a400-158d5e9158ce', N'Pizza', N'chicken, chilli sauce,black pepper, salt, vinegar, soy sause, garlic, olive, piza sause, mozzarella cheese, oregano, mushrooms, tomato, capsicum, flour, yeast,egg, oil', CAST(99.99 AS Decimal(18, 2)), N'https://www.pakistanichefrecipes.com/wp-content/uploads/2018/04/Chicken-Fajita-Pizza-500x500.jpg')
+GO
+INSERT [dbo].[FoodProducts] ([Id], [Name], [Description], [Price], [ImageUrl]) VALUES (N'44f13a20-0687-4d3b-b57f-301f797994bd', N'Vegan Sandwich', N'slices bread-toasted,slices tomato,light mayonnaise mixed with wasabi,iceberg lettuce leaves,mozzarella cheese', CAST(59.99 AS Decimal(18, 2)), N'https://www.simplyrecipes.com/wp-content/uploads/2019/10/Vegan-Lentil-Sloppy-LEAD-1-600x840.jpg')
+GO
+INSERT [dbo].[FoodProducts] ([Id], [Name], [Description], [Price], [ImageUrl]) VALUES (N'2eeb26c1-6ce7-4d2e-aef4-4f4393a76600', N'Cheeseburger', N'Becon,onion, Beef,Salad, tomato, cheese', CAST(69.99 AS Decimal(18, 2)), N'https://www.simplyrecipes.com/wp-content/uploads/2019/04/Stovetop-Cheeseburgers-hero1_v2-600x841.jpg')
+GO
+INSERT [dbo].[FoodProducts] ([Id], [Name], [Description], [Price], [ImageUrl]) VALUES (N'31c46e70-8d8b-4629-8047-8266579945f5', N'Pasta', N'butter,shallot,garlic, tomato paste,red pepper flakes,vodka,Kosher salt,pasta, penne or rigatoni,,heavy cream, Parmesan, Basil', CAST(89.99 AS Decimal(18, 2)), N'https://www.simplyrecipes.com/wp-content/uploads/2012/02/pasta-carbonara-vertical-a-1200-600x892.jpg')
+GO
+INSERT [dbo].[FoodProducts] ([Id], [Name], [Description], [Price], [ImageUrl]) VALUES (N'3a182357-6233-4eef-9e5d-9389ef00088b', N'Salad', N'Seafrost Salt and Pepper Squid,Soy sauce,Mirin,Mixed salad leaves,Wakame seaweed', CAST(39.99 AS Decimal(18, 2)), N'https://www.simplyrecipes.com/wp-content/uploads/2007/10/waldorf-salad-vertical-a3-1500-600x837.jpg')
+GO
+INSERT [dbo].[FoodProducts] ([Id], [Name], [Description], [Price], [ImageUrl]) VALUES (N'd8a90840-ffc0-4bc1-9579-a0711f33ce9f', N'Fried Chicken Wings', N'chicken, flour, Parmesan cheese, paprika, dry mustard, dried oregano, crumbled,salt, black pepper,milk, Peanut oil', CAST(89.99 AS Decimal(18, 2)), N'https://www.simplyrecipes.com/wp-content/uploads/2019/09/Cacio-e-Pepe-Chicken-Wings-Lead-8-600x840.jpg')
+GO
+INSERT [dbo].[FoodProducts] ([Id], [Name], [Description], [Price], [ImageUrl]) VALUES (N'da4f0902-9313-4dde-a8c4-cd7e7cadb49c', N'Broccoli Cheddar Soup', N'Butter,yellow onion, carrot,broccoli, garlic, flour,chicken, beer, milk, Cheddar cheese,Salt and black pepper to taste,Tabasco sauce, Parmesan crisps', CAST(79.99 AS Decimal(18, 2)), N'https://www.simplyrecipes.com/wp-content/uploads/2018/02/Broccoli-Cheddar-Soup-LEAD-3-600x840.jpg')
+GO
---
+++ Lets.WebService.Test/OrderTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lets.WebService.Test
+{
+    public class OrderTests
+    {
+
+    }
+}
